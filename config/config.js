72dbf8d720e68c61cddf7a285af893233d93a927
Jigme-Lhuendrup <07230029.sherubtse@rub.edu.bt>
2025-06-05 20:13:37 +0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 
+const { Sequelize } = require('sequelize');
+const config = require('./config')[process.env.NODE_ENV || 'development'];
+
+const sequelize = new Sequelize(config);
+module.exports = sequelize;
+
 module.exports = {
   development: {
     username: process.env.DB_USER || 'postgres',
